cb0fb0d4a24ee939223a23966e459a54fc3f8572
Sumin Son <clvswft03@gmail.com>
2022-02-02 10:45:09 +0900

    feat(example): add java-maven examples

---
+++ examples/java-maven/src/test/java/com/example/myproject/TestApp.java
@@ -0,0 +1,23 @@
+package com.example.myproject;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ * Tests for correct dependency retrieval with maven rules.
+ */
+public class TestApp {
+
+  @Test
+  public void testCompare() throws Exception {
+    App app = new App();
+    assertEquals("should return 0 when both numbers are equal", 0, app.compare(1, 1));
+  }
+
+  @Test
+  public void testCompare2() throws Exception {
+    App app = new App();
+    assertEquals("should return 0 when both numbers are equal", 0, app.compare(1, 1));
+  }
+
+}
